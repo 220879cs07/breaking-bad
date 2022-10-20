36e5bb57f09a9a071e896b2e4305ac7ba8372a19
iyatx <azizbeksavkimov@mail.ru>
2022-10-20 08:41:32 +0500

    fix: config

---
+++ src/shared/Layout/Layout.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+interface Props {
+}
+
+const HelloWorld: FC<Props> = () => {
+  return <></>;
+};
+
+export default HelloWorld;
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
   resolve: {
     alias: [
       { find: 'api', replacement: path.resolve(__dirname, 'src/api') },
