e2199c2a8078d3b4c1b970fed2fb07318e0a2989
aza-me <220879@akfauniversity.org>
2022-11-15 13:14:01 +0500

    feat: link more

--- src/pages/EpisodesPage/components/EpisodesCard/EpisodesCard.tsx
+++ src/pages/EpisodesPage/components/EpisodesCard/EpisodesCard.tsx
@@ -3,6 +3,7 @@ import cn from 'app/helpers/cn';
 import { formatDate } from 'app/helpers/formatDate';
 import { EpisodeModel } from 'app/models/EpisodesModel';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './EpisodesCard.sass';
 
 interface Props {
@@ -11,16 +12,20 @@ interface Props {
 }
 
 const EpisodesCard: React.FC<Props> = ({ episode, className }) => (
-  <Card className={cn('episode-card', className)}>
-    <CardContent>
-      <Chip className='episode-card__label' label={episode.series} color='success' />
-      <h3 className='episode-card__title'>{episode.title}</h3>
-      <span className='episode-card__date'>{formatDate({ date: episode.air_date })}</span>
-      <div className='episode-card__characters'>
-        <span className='episode-card__characters-title'>Characters:</span> {episode.characters.join(', ')}
-      </div>
-    </CardContent>
-  </Card>
+  <div className='episode-card'>
+    <Chip className='episode-card__label' label={episode.series} color='success' />
+    <h3 className='episode-card__title'>{episode.title}</h3>
+    <span className='episode-card__date'>{formatDate({ date: episode.air_date })}</span>
+    <div className='episode-card__characters'>
+      <span className='episode-card__characters-title'>Characters:</span> {episode.characters.join(', ')}
+    </div>
+
+    <div className='episode-card__footer'>
+      <Link to={`/episodes/${episode.episode_id}`} className='episode-card__more'>
+        More details
+      </Link>
+    </div>
+  </div>
 );
 
 export default EpisodesCard;
